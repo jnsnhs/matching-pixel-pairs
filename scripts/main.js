a24e53a666c57dc527d1fe63cd485e16c8d74f03
Jens Neuhaus <neuhaus@posteo.de>
2026-05-01 17:42:20 +0200

    Refactoring: everything is OOP now.

--- scripts/main.js
+++ scripts/main.js
@@ -1,90 +1,115 @@
-let images = 24;
-let gameSize = 48;
-let folder = 'images';
-let cardStyle = 'junior';
-let cardSize = 'large';
+const imgDir = "./images";
+const parentElement = document.getElementById("game");
 
-dealCards();
-
-function dealCards() {
-	images = sequence(1,images);
-	images = shuffle(images);
-	images = images.slice(0,gameSize/2);
-	images = images.concat(images);
-	images = shuffle(images);
-	let grid = document.createElement('ul')
-	grid.classList.add(cardSize);
-	grid.classList.add('grid');
-	for (let i = 0; i < gameSize; i++) {
-		let card = document.createElement('li');
-		card.classList.add('card');
-		card.classList.add(cardStyle);
-		let front = document.createElement('div');
-		front.classList.add('front');
-		front.style.backgroundImage = 'url("./'+folder+'/'+images[i]+'.jpg")';
-		let back = document.createElement('div');
-		back.classList.add('back');
-		back.style.backgroundImage = 'url("./'+folder+'/'+ 'bg.jpg")';
-		card.addEventListener('click', chooseCard);
-		card.appendChild(back);
-		card.appendChild(front);
-		grid.appendChild(card);
-	}
-	document.getElementById('table').appendChild(grid);
-}
-
-let firstChoice = null;
-let secondChoice = null;
-
-function chooseCard(event) {
-	if (firstChoice == null && this.classList.contains('flipped') != true) {
-		firstChoice = this;
-		flipCard(this);
-	} else if (secondChoice == null && this.classList.contains('flipped') != true) {
-		secondChoice = this;
-		flipCard(this);
-		if (firstChoice.lastChild.style.backgroundImage == secondChoice.lastChild.style.backgroundImage) {
-			firstChoice.classList.add('found');
-			secondChoice.classList.add('found');
-			firstChoice = null;
-			secondChoice = null;
-		} 
-	} else if (this.classList.contains('flipped') != true) {
-			flipCard(firstChoice);
-			flipCard(secondChoice);
-			firstChoice = null;
-			secondChoice = null;
-			flipCard(this);
-			firstChoice = this;
-	}
-}
-
-function flipCard(card) {
-	if (card.classList.contains('flipped') == false) {
-		card.classList.add('flipped');
-		card.style.transform = 'rotateY(180deg)';
-	} else {
-		card.classList.remove('flipped');
-		card.style.transform = 'rotateY(0deg)';
-	}
-}
-
-function sequence(from, to) {
-	let result = [];
-	for (let i = from; i < to + 1; ++i) {
-		result.push(i);
-	}
-	return result;
-}
-
-function shuffle(array) {
-  let currentIndex = array.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+class Utils {
+  static getShuffledArray(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
-  return array;
 }
+
+class Game {
+  constructor(numberOfCards, debugMode = false) {
+    this.numberOfCards = numberOfCards;
+    this.roundsPlayed = 0;
+    this.debugMode = debugMode;
+    this.firstChoiceCard = null;
+    this.secondChoiceCard = null;
+    this.listOfimageUrls = this.setListOfimageUrls(numberOfCards);
+    if (debugMode) {
+      this.displayDebugInfo();
+    }
+  }
+  setListOfimageUrls(numberOfCards) {
+    let imageUrls = [];
+    for (let i = 0; i < numberOfCards / 2; i++) {
+      let fileName = `${i + 1}.jpg`;
+      imageUrls.push(fileName);
+    }
+    return Utils.getShuffledArray([...imageUrls, ...imageUrls]);
+  }
+  createCards() {
+    let grid = document.createElement("ul");
+    grid.classList.add("grid");
+    for (let i = 0; i < this.numberOfCards; i++) {
+      let card = document.createElement("li");
+      card.classList.add("card");
+      let front = document.createElement("div");
+      front.classList.add("front");
+      front.style.backgroundImage =
+        'url("' + imgDir + "/" + this.listOfimageUrls[i] + '")';
+      let back = document.createElement("div");
+      back.classList.add("back");
+      card.addEventListener("click", () => {
+        this.chooseCard(card);
+      });
+      card.appendChild(back);
+      card.appendChild(front);
+      grid.appendChild(card);
+    }
+    parentElement.appendChild(grid);
+  }
+  chooseCard(clickedCard) {
+    let isCardCovered = !clickedCard.classList.contains("uncovered");
+    console.log(isCardCovered);
+    if (!this.firstChoiceCard && isCardCovered) {
+      this.roundsPlayed++;
+      this.firstChoiceCard = clickedCard;
+      this.flipCard(clickedCard);
+    } else if (!this.secondChoiceCard && isCardCovered) {
+      this.secondChoiceCard = clickedCard;
+      this.flipCard(clickedCard);
+      if (
+        this.firstChoiceCard.lastChild.style.backgroundImage ==
+        this.secondChoiceCard.lastChild.style.backgroundImage
+      ) {
+        this.firstChoiceCard.classList.add("found");
+        this.secondChoiceCard.classList.add("found");
+        this.firstChoiceCard = null;
+        this.secondChoiceCard = null;
+      }
+    } else if (isCardCovered) {
+      this.flipCard(this.firstChoiceCard);
+      this.flipCard(this.secondChoiceCard);
+      this.firstChoiceCard = null;
+      this.secondChoiceCard = null;
+      this.firstChoiceCard = clickedCard;
+      this.flipCard(clickedCard);
+    }
+    if (this.debugMode) {
+      this.displayDebugInfo();
+    }
+  }
+  flipCard(cardElement) {
+  if (cardElement.classList.contains("uncovered")) {
+    cardElement.classList.remove("uncovered");
+    // Replace with class:
+    //cardElement.style.transform = "rotateY(0deg)";
+  } else {
+    cardElement.classList.add("uncovered");
+    // Replace with class:
+    //cardElement.style.transform = "rotateY(180deg)";
+  }
+}
+  displayDebugInfo() {
+    let debugContainer = document.getElementById("debug");
+    if (!debugContainer) {
+      debugContainer = document.createElement("div");
+      debugContainer.id = "debug";
+    }
+    debugContainer.innerHTML = `Rounds played: ${this.roundsPlayed}`;
+    document.body.appendChild(debugContainer);
+  }
+}
+
+let game = new Game(48);
+game.createCards();
